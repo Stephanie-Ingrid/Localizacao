fe33e2bf3149c64c84521f9018d18c49a8c19fd8
Stephanie Ingrid <stephanieingrid5@gmail.com>
2023-10-06 16:44:51 -0300

    Query Methods

--- src/main/java/io/github/stephanieingrid/localizacao/domain/repository/CidadeRepository.java
+++ src/main/java/io/github/stephanieingrid/localizacao/domain/repository/CidadeRepository.java
@@ -1,7 +1,25 @@
 package io.github.stephanieingrid.localizacao.domain.repository;
 
+import ch.qos.logback.classic.pattern.LineSeparatorConverter;
 import io.github.stephanieingrid.localizacao.domain.entity.Cidade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CidadeRepository extends JpaRepository<Cidade, Long> {
+
+
+    List<Cidade> findByNome(String nome);
+
+    List<Cidade> findByNomeStartingWith(String nome);
+
+    List<Cidade> findByNomeEndingWith(String nome);
+
+    List<Cidade> findByNomeContaining(String nome);
+
+
+
+    List<Cidade> findByHabitantes(Long habitantes);
+
 }
