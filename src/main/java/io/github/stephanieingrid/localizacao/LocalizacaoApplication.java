0dd3439434eca9b097b897f332115e9b93b34882
Stephanie Ingrid <stephanieingrid5@gmail.com>
2023-10-06 15:43:44 -0300

    metodos para salvar e listar

--- src/main/java/io/github/stephanieingrid/localizacao/LocalizacaoApplication.java
+++ src/main/java/io/github/stephanieingrid/localizacao/LocalizacaoApplication.java
@@ -1,11 +1,32 @@
 package io.github.stephanieingrid.localizacao;
 
+import io.github.stephanieingrid.localizacao.domain.entity.Cidade;
+import io.github.stephanieingrid.localizacao.domain.repository.CidadeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
-public class LocalizacaoApplication {
+public class LocalizacaoApplication implements CommandLineRunner {
 
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		listarCidades();
+	}
+	@Transactional
+	void salvarCidade(){
+
+		var cidade = new Cidade( 1L, "São Paulo",11451245L );
+		cidadeRepository.save(cidade);
+	}
+	void listarCidades(){
+		cidadeRepository.findAll().forEach(System.out::println);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(LocalizacaoApplication.class, args);
 	}
